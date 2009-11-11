b22bd86fbc390cd4436607d839e59756ed5205dd
karlchen321 <karlchen321@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-11 21:34:42 +0000

    version update auf 1.0.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/lock_ts/trunk@26431 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "lock_ts"
 #
-# Auto generated 30-09-2009 19:48
+# Auto generated 11-11-2009 22:20
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -14,22 +14,24 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Lock TypoScript Templates',
 	'description' => 'A simple write protection for TypoScript Templates.',
 	'category' => 'be',
-	'author' => 'Sven Juergens',
-	'author_email' => 'post@t3area.de',
-	'shy' => '',
-	'dependencies' => 'cms',
+	'shy' => 0,
+	'version' => '1.0.1',
+	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
+	'loadOrder' => '',
 	'module' => '',
 	'state' => 'beta',
-	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
+	'clearcacheonload' => 0,
 	'lockType' => '',
+	'author' => 'Sven Juergens',
+	'author_email' => 'post@t3area.de',
 	'author_company' => '',
-	'version' => '1.0.0',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2-0.0.0',
@@ -40,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"229a";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"6a1b";s:14:"ext_tables.sql";s:4:"51ea";s:16:"locallang_db.xml";s:4:"ea0b";s:19:"doc/wizard_form.dat";s:4:"2fc6";s:20:"doc/wizard_form.html";s:4:"9bf5";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"229a";s:10:"README.txt";s:4:"ee2d";s:25:"class.tx_lockts_hooks.php";s:4:"d7af";s:12:"ext_icon.gif";s:4:"4c12";s:14:"ext_tables.php";s:4:"bcec";s:14:"ext_tables.sql";s:4:"51ea";s:16:"locallang_db.xml";s:4:"1a55";s:14:"doc/manual.sxw";s:4:"0378";s:19:"doc/wizard_form.dat";s:4:"2fc6";s:20:"doc/wizard_form.html";s:4:"9bf5";}',
 );
 
 ?>
